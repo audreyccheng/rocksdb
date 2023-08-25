e3b2f87e5ab2c8bbbcaeca827d48201491e75d43
Audrey Cheng <audreycc@fb.com>
2023-08-25 00:06:55 -0700

    scheduling

--- utilities/transactions/optimistic_transaction.cc
+++ utilities/transactions/optimistic_transaction.cc
@@ -96,7 +96,8 @@ Status OptimisticTransaction::Schedule(int type) {
 
 Status OptimisticTransaction::GetKey(const ReadOptions& options, const Slice& key, std::string* value) {
   std::string key_byte(key.data());
-  int key_val = stoi(key_byte.substr(0,19));
+  std::cout << "Get key: " << key_byte << std::endl;
+  int key_val = stoi(key_byte.substr(0,15));
   std::string key_str = std::to_string(key_val);
   std::cout << "Get key: " << key_str  << " tid: " << this->GetIndex() << std::endl;
   bool get_success = false;
@@ -153,7 +154,8 @@ Status OptimisticTransaction::GetForUpdateKey(const ReadOptions& options, const 
                               std::string* value, bool exclusive,
                               const bool do_validate) {
   std::string key_byte(key.data());
-  int key_val = stoi(key_byte.substr(0,19));
+  std::cout << "GetForUpdate key: " << key_byte << std::endl;
+  int key_val = stoi(key_byte.substr(0,15));
   std::string key_str = std::to_string(key_val);
   std::cout << "GetForUpdate key: " << key_str << " tid: " << this->GetIndex() << std::endl;
   bool get_success = false;
@@ -201,7 +203,8 @@ Status OptimisticTransaction::GetForUpdateKey(const ReadOptions& options, const 
 
 Status OptimisticTransaction::PutKey(const Slice& key, const Slice& value) {
   std::string key_byte(key.data());
-  int key_val = stoi(key_byte.substr(0,19));
+  std::cout << "Put key: " << key_byte << std::endl;
+  int key_val = stoi(key_byte.substr(0,15));
   std::string key_str = std::to_string(key_val);
   std::cout << "Put key: " << key_str << " tid: " << this->GetIndex() << std::endl;
   auto txn_db_impl = static_cast_with_check<OptimisticTransactionDBImpl,
